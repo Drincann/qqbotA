412b931d1885d6b74bcfc5f4d5501bf5d47324d4
gaolihai <1019933576@qq.com>
2022-01-15 22:13:34 +0800

    feature: config

---
+++ config.ts
@@ -0,0 +1,12 @@
+export const config = {
+    groupList: [
+        829153782
+    ],
+    qq: 3070539027,
+    baseUrl: 'http://localhost:8080',
+    verifyKey: '123456789',
+    imagePathRoot: './image',
+    moliApiKey: 'moli',
+    moliApiSecret: 'moli',
+    turingApiKey: 'turing',
+}
