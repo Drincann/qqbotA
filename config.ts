031c7d486bc047cb0d8cb3396191ae5f7067eed7
gaolihai <1019933576@qq.com>
2022-01-16 13:03:00 +0800

    feature: 图片 service 的配置

--- config.ts
+++ config.ts
@@ -9,4 +9,6 @@ export const config = {
     moliApiKey: 'moli',
     moliApiSecret: 'moli',
     turingApiKey: 'turing',
+    miraiconsoleImageRoot: './image',
+    processImageRoot: '../Mirai/mcl/image',
 }
